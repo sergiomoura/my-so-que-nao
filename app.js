45af89b828ec63e4f03747ae75d4d28c079a4b15
Wendel Cutrim <wendelcutrim@hotmail.com>
2022-05-23 11:29:02 -0300

    Aula 23/05. Realizando busca de usuário via query string utilizando operadores do squelize

--- app.js
+++ app.js
@@ -3,18 +3,18 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
 var UsuariosRouter = require('./routes/UsuariosRouter');
 
 var app = express();
 
+//Server config
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+//Routes
 app.use('/api/v1/usuarios', UsuariosRouter);
 
 module.exports = app;
--- controllers/UsuariosController.js
+++ controllers/UsuariosController.js
@@ -1,5 +1,7 @@
 const { Usuario } = require('../models');
-const UsuariosController = {
+const { Op } = require('sequelize');
+
+module.exports = {
     registrar: async (req, res) => {
         //Criar usuário
         res.send('Vai criar um usuario...')
@@ -7,13 +9,29 @@ const UsuariosController = {
 
     buscar: async (req, res) => {
         try{
+            //Capturar o trecho que está sendo buscado
+
+            let trechoBuscado = req.query.q;
+            if(trechoBuscado){
+                //Carregar os usuários que tenham o trecho busado no nome
+                // SELECT * FROm usuarios WHERE nome like(%'{trechoBuscado}%');
+                const usuario = await Usuario.findAll({
+                    where: {nome: {[Op.substring]: trechoBuscado}}
+                });
+    
+                //Enviar para o cliente os usuarios leventados
+    
+                return res.send(usuario);
+    
+                //Consulta: http://localhost:3000/api/v1/usuarios?q=1
+            }
+
             const usuario = await Usuario.findAll();
             return res.send(usuario);
+
         } catch (error){
             console.log(error);
             return res.send({message: "Erro"});
         }
     }
 };
-
-module.exports = UsuariosController;
