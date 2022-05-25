9e2ef64f6edd97eb411f667645d1bd87d9c6fe2f
Wendel Cutrim <wendelcutrim@hotmail.com>
2022-05-25 11:36:49 -0300

    Aula 25/05. Criado o endpoint para registrar usuários e sua documentação no postman

--- controllers/UsuariosController.js
+++ controllers/UsuariosController.js
@@ -1,10 +1,39 @@
 const { Usuario } = require('../models');
 const { Op } = require('sequelize');
+const bcrypt = require('bcrypt');
 
 module.exports = {
     registrar: async (req, res) => {
-        //Criar usuário
-        res.send('Vai criar um usuario...')
+        try {
+            //Capturar os dados do Usuário que foi enviado pelo req.body;
+            const { nome, email, senha, foto } = req.body;
+
+            //Criptografar a senha do usuário
+            const hash = bcrypt.hashSync(senha, 10);
+
+            //Verificar se o usuário já tem cadastro no nosso banco de dados
+            const verificarUsuarioCadastrado = await Usuario.findOne({where: {email: email}});
+
+            //Barrar o cadastro do usuário caso já esteja cadastrado no banco de dados.
+            if(verificarUsuarioCadastrado){
+                return res.status(409).json({error: 'Falha na autenticação'});
+            }
+
+            //Cadastrar um novo usuário no banco de dados.
+            const novoUsuario = await Usuario.create(
+                {
+                    nome,
+                    email,
+                    senha: hash,
+                    foto
+                });
+
+            //Retorna um JSON com os dados do usuário cadastrado.
+            return res.status(201).json(novoUsuario);
+        } catch (error){
+            console.log(error);
+            return res.status(401).json({error})
+        }
     },
 
     buscar: async (req, res) => {
