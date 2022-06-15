ed69971598ada05c96a0cf74d1e9f361218581ba
Sérgio Moura <smouracalmon@gmail.com>
2022-06-15 10:30:27 -0300

    Realizando upload de imagem via fetch sem middleware armengueiro

--- middlewares/UploadDeAvatar.js
+++ middlewares/UploadDeAvatar.js
@@ -1,5 +1,15 @@
 const multer = require('multer');
-const upload = multer({dest:'public/img/avatares/'});
 
+const storage = multer.diskStorage(
+    {
+        destination:'public/img/avatares/',
+        filename: function(req, file, callback){
+            let novoNome = `${Math.random()}-${file.originalname}`;
+            callback(null, novoNome);
+        }
+    }
+)
+
+const upload = multer({storage});
 
 module.exports = upload.single('foto');
