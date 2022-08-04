002d384aab6e7014b210e0ef189eee70a559e291
Sérgio Moura <smouracalmon@gmail.com>
2022-08-04 17:25:29 -0300

    Rota e controller funcionando

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 const express = require('express');
+const UsuariosRouter = require('./routes/UsuariosRouter');
+
 const app = express();
+
+app.use('/', UsuariosRouter)
 
 app.listen(3000,()=>{console.log("Estou escutando na porta 3000")})
---
+++ routes/UsuariosRouter.js
@@ -0,0 +1,16 @@
+// Importar o express
+const express = require('express');
+const UsuariosController = require('../controllers/UsuariosController')
+
+// Criar o roteador
+const router = express.Router();
+
+// Definir a rota get para /create
+// Essa deve direcionar a requisição para UsuariosController.create (ainda inexiste)
+router.get(
+    '/create',
+    UsuariosController.create
+)
+
+// Expostar o roteador e UTILIZÁ-LO no app.js
+module.exports = router;
